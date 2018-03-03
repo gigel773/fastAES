00b3d4d7e503bb000bb07d1268e5c02ca3232a0a
Gigel773 <gigelbig@gmail.com>
2018-03-03 14:17:32 +0300

    Documentation for library functions done

--- lib/sources/utils/include/ownAES.h
+++ lib/sources/utils/include/ownAES.h
@@ -5,16 +5,18 @@
 #include <wmmintrin.h>
 
 /**
+ * Generates encryption keys according to AES algorithm
  *
  * @param initialKey
- * @param encryptionKeys
+ * @param encryptionKeys pointer to array where result keys should be stored (must be 11 elements long)
  */
 void ownGenerateEncryptionKeys(const signed char *initialKey, __m128i *encryptionKeys);
 
 /**
+ * Generates decryption keys according to AES algorithm
  *
- * @param encryptionKeys
- * @param decryptionKeys
+ * @param encryptionKeys pointer to existing encryption keys (must be 11 elements long)
+ * @param decryptionKeys pointer to array where result keys should be stored (must be 10 elements long)
  */
 void ownGenerateDecryptionKeys(__m128i *encryptionKeys, __m128i *decryptionKeys);
 
