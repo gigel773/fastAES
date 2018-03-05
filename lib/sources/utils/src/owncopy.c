2cbe2d438bd22debde5d5ac4d1a2285b6cd446ae
Gigel773 <gigelbig@gmail.com>
2018-03-05 23:24:33 +0300

    Initial release

--- lib/sources/utils/src/owncopy.c
+++
@@ -1,9 +0,0 @@
-#include "owns.h"
-
-void ownCopy_8s(const signed char *pSrc, signed char *pDst, int len) {
-#pragma GCC ivdep
-    for (int i = 0; i < len; i++) {
-        pDst[i] = pSrc[i];
-    }
-}
-
